4d6640f06592343af6d8c2d899d683c04663e259
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-23 00:19:22 -0700

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { FaGithub, FaInstagram, FaLinkedinIn, FaEnvelope, FaFileAlt } from "react-icons/fa"
+import Wave from 'react-wavify'
 
 const Footer = () => {
     return (
-        <div className="w-full flex justify-center items-center">
-            <div className="w-1/2 flex justify-evenly items-center text-5xl">
-                <FaGithub />
-                <FaInstagram />
-                <FaLinkedinIn />
-                <FaEnvelope />
-                <FaFileAlt />
+        <div className="w-full flex justify-center items-center flex-col">
+            <Wave fill='black'
+                className='fill-black'
+                paused={false}
+                options={{
+                    height: 20,
+                    amplitude: 30,
+                    speed: 0.25,
+                    points: 3
+                }}
+            />
+            <div className='w-full bg-black flex justify-center items-center'>
+            <div className="w-1/2 text-white flex justify-evenly items-center text-5xl p-5">
+                    <FaGithub />
+                    <FaInstagram />
+                    <FaLinkedinIn />
+                    <FaEnvelope />
+                    <FaFileAlt />
+                </div>
             </div>
+
+                
         </div>
     )
 }
