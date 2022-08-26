2be7ba587db95c0e98f4984942e44b2909bdd8be
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-26 00:16:28 -0700

    shooting stars

--- components/Landing.js
+++ components/Landing.js
@@ -1,32 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { Row, Col } from "react-bootstrap"
 import { FaGithub, FaInstagram, FaLinkedinIn, FaEnvelope, FaFileAlt } from "react-icons/fa"
 
+const createStars = (starsToCreate) => {
+    const array = Array(250)
+    for (let i = 0; i < starsToCreate; i++) {
+        array[i] = {
+            top: `${Math.floor(Math.random() * 100).toString()}%`,
+            left: `${Math.floor(Math.random() * 100).toString()}%`
+        }
+    }
+    return array
+}
+
 
 const Landing = () => {
+    const [shooting_stars, setShootingStars] = useState([])
 
-    const stars = new Array(250).fill(0)
+    const largeStars = useMemo(() => createStars(250), []);
+    const smallStars = useMemo(() => createStars(250), []);
 
-
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setShootingStars(Array(Math.floor((Math.random() * 4) + 1)).fill(0))
+        }, 4000);
+      
+        return () => clearInterval(interval);
+      }, []);
 
     return (
         <div style={{ height: "100vh" }} className="relative flex justify-center items-center bg-gradient-to-b from-sky-900 to-purple-500 ">
 
             {
-                stars.map((star, index) => (
-                    <span style={{ top: (Math.random() * 100).toString() + "%", left: (Math.random() * 100).toString() + "%" }}
+                largeStars.map((star, index) => (
+                    <span style={{ top: star.top, left: star.left}}
                         key={index}
                         className="absolute animate-pulse bg-white h-[2px] w-[2px] rounded-full"></span>
                 ))
             }
             {
-                stars.map((star, index) => (
-                    <span style={{ top: (Math.random() * 100).toString() + "%", left: (Math.random() * 100).toString() + "%" }}
+                smallStars.map((star, index) => (
+                    <span style={{ top: star.top, left: star.left}}
                         key={index}
                         className="absolute animate-twinkling-star bg-white h-1 w-1 rounded-full"></span>
                 ))
             }
-
+            {
+                shooting_stars.map((star, index) => (
+                    <div key={index} className="absolute" style={{ top: 0, left: (Math.random() * 75).toString() + "%" }}>
+                        <span className="animate-shooting-star shadow-shooting-star absolute h-1 w-1 bg-white top-0 left-0 rounded-full -translate-x-1/2 -translate-y-1/2 origin-top-left"></span>
+                        <span className="animate-shooting-star trail bg-gradient-to-r from-white to-transparent content-none absolute origin-top-left -top-0.5 left-0 -translate-y-1/2 w-80 h-0.5"></span>
+                    </div>
+                ))
+            }
             <Row className='flex justify-center items-center'>
                 <Col lg={6} className="flex justify-center items-center flex-col">
                     <p className="font-lexend text-6xl text-center text-white">Divyank Shah<br /><p className='text-3xl mt-3'>Aspiring Software Engineer</p></p>
@@ -42,11 +68,7 @@ const Landing = () => {
                     <img className="w-4/6 rounded-lg z-10" src="profile.jpg" />
 
                 </Col>
-
             </Row>
-
-
-
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,18 @@ module.exports = {
             lexend: ["Lexend"],
         },
         keyframes : {
-            twinkle : {
-                "50%" : {opacity: ".5"},
+            translate : {
+                "0%" : {transform: "rotate(-135deg) translateX(0)"},
+                "100%" : {transform: "rotate(-135deg) translateX(-2000px)"},
             }
         },
         animation : {
-            'twinkling-star': 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite;'
-        }
-
+            'twinkling-star': 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite;',
+            'shooting-star' : 'translate 4s linear 1;'
+        },
+        boxShadow: {
+            'shooting-star': '0 0 0 4px rgba(255, 255, 255, .1)'
+        },
     },
   },
   plugins: [],
